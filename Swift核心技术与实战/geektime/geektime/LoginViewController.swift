f61ed3828cca0809395116a0200fd009f4adf091
stonesun1314 <stonesun1314@163.com>
2020-01-05 19:52:17 +0800

    完成了登录页

--- Swift核心技术与实战/geektime/geektime/LoginViewController.swift
+++ Swift核心技术与实战/geektime/geektime/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class LoginViewController: BaseViewController {
     
@@ -18,9 +19,73 @@ class LoginViewController: BaseViewController {
 
         // Do any additional setup after loading the view.
         
-//        let logoView = UIImageView(image: R.image.logo())
+        let logoView = UIImageView(image: R.image.logo())
+        view.addSubview(logoView)
+        
+        logoView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(30)
+            make.centerX.equalToSuperview()
+        }
+        
+        let phoneIconView = UIImageView(image: R.image.icon_phone())
+        phoneTextField = UITextField()
+        phoneTextField.leftView = phoneIconView
+        phoneTextField.leftViewMode = .always
+        phoneTextField.layer.borderColor = UIColor.hexColor(0x333333).cgColor
+        phoneTextField.layer.borderWidth = 1
+        phoneTextField.textColor = UIColor.hexColor(0x333333)
+        phoneTextField.layer.cornerRadius = 5
+        phoneTextField.layer.masksToBounds = true
+        phoneTextField.placeholder = "请输入手机号"
+        view.addSubview(phoneTextField)
+        
+        phoneTextField.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.top.equalTo(logoView.snp_bottom).offset(20)
+            make.height.equalTo(50)
+        }
+        
+        let passwordIconView = UIImageView(image: R.image.icon_pwd())
+        passwordTextField = UITextField()
+        passwordTextField.leftView = passwordIconView
+        passwordTextField.leftViewMode = .always
+        passwordTextField.layer.borderColor = UIColor.hexColor(0x333333).cgColor
+        passwordTextField.layer.borderWidth = 1
+        passwordTextField.textColor = UIColor.hexColor(0x333333)
+        passwordTextField.layer.cornerRadius = 5
+        passwordTextField.layer.masksToBounds = true
+        passwordTextField.placeholder = "请输入手机号"
+        view.addSubview(phoneTextField)
+        phoneTextField.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.top.equalTo(logoView.snp_bottom).offset(20)
+        }
+        
+        let loginButton = UIButton(type: .custom)
+        loginButton.setTitle("登录", for: .normal)
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        loginButton.setBackgroundImage(UIColor.hexColor(0xf8892e).toImage(), for: .normal)
+        
+        loginButton.layer.cornerRadius = 5
+        loginButton.layer.masksToBounds = true
+        view.addSubview(loginButton)
+        loginButton.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.top.equalTo(passwordTextField.snp_bottom).offset(20)
+            make.height.equalTo(50)
+        }
+        
+        loginButton.addTarget(self, action: #selector(didClickLoginButton), for: .touchUpInside)
+        
     }
     
+    @objc func didClickLoginButton() {
+        NSLog("didClickLoginButton")
+    }
 
     /*
     // MARK: - Navigation
