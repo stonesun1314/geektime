2ce57dbc35d38db8dc7874c02fd2d082a1ac5ea0
stonesun1314 <stonesun1314@163.com>
2020-01-06 21:43:11 +0800

    commit

--- Swift核心技术与实战/geektime/geektime/Component/BannerView.swift
+++ Swift核心技术与实战/geektime/geektime/Component/BannerView.swift
@@ -12,7 +12,7 @@ import SnapKit
 protocol BannerViewDataSource: AnyObject {
     func numberOfBanners(_ bannerView: BannerView) -> Int
     
-    func viewForBanner(_ bannerView: BannerView, index: Int, convertView: UIView) -> UIView
+    func viewForBanner(_ bannerView: BannerView, index: Int, convertView: UIView?) -> UIView
 }
 
 protocol BannerViewDelegate: AnyObject {
@@ -34,10 +34,48 @@ class BannerView: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
     var isInfinite : Bool = true
     var timer: Timer?
     
+    static var cellId = "bannerViewCellId"
+    static var convertViewTag = 10086
+    
     override init(frame: CGRect) {
-//        flowLayout = UICollectionViewFlowLayout()
-//        flowLayout.headerReferenceSize = .zero
-//        flowLayout.footerReferenceSize = .zero
+        flowLayout = UICollectionViewFlowLayout()
+        flowLayout.headerReferenceSize = .zero
+        flowLayout.footerReferenceSize = .zero
+        flowLayout.minimumLineSpacing = 0
+        flowLayout.minimumInteritemSpacing = 0
+        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        flowLayout.scrollDirection = .horizontal
+        
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height), collectionViewLayout: flowLayout)
+        pageControl = UIPageControl()
+        super.init(frame: frame)
+        
+        self.setupViews()
+        
+        
+    }
+    
+    func setupViews() -> Void {
+        collectionView.backgroundColor = .white
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.contentInsetAdjustmentBehavior = .never
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: BannerView.cellId)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        self.addSubview(collectionView)
+        self.addSubview(pageControl)
+        
+        collectionView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+        
+        pageControl.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-15)
+        }
     }
     
     required init?(coder: NSCoder) {
@@ -45,11 +83,74 @@ class BannerView: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
+        let pageNumber = dataSource.numberOfBanners(self)
+        if isInfinite {
+            if pageNumber == 1 {
+                return 1
+            } else {
+                return pageNumber + 2
+            }
+        } else {
+            return pageNumber
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BannerView.cellId, for: indexPath)
+        var index = indexPath.row
+        if isInfinite {
+            let pageNumber = dataSource.numberOfBanners(self)
+            if pageNumber > 1 {
+                if indexPath.row == 0 {
+                    index = pageNumber - 1
+                } else if indexPath.row == pageNumber + 1 {
+                    index = 0
+                } else {
+                    index = indexPath.row - 1
+                }
+            }
+        }
         
+        if let view = cell.contentView.viewWithTag(BannerView.convertViewTag) {
+            let _ = dataSource.viewForBanner(self, index: index, convertView: view)
+        }else {
+            let newView = dataSource.viewForBanner(self, index: index, convertView: nil)
+            newView.tag = BannerView.convertViewTag
+            cell.contentView.addSubview(newView)
+            newView.snp.makeConstraints { (make) in
+                make.edges.equalToSuperview()
+            }
+        }
+        
+        return cell
+    }
+    
+    func startAutoScroll() {
+        guard autoScrollInterval > 0 && timer == nil else {
+            return
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: TimeInterval(autoScrollInterval), target: self, selector: #selector(flipNext), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer!, forMode: .common)
+    }
+    
+    @objc func flipNext() {
+        guard let _ = superview, let _ = window else {
+            return
+        }
+        
+        let totalPageNumber = dataSource.numberOfBanners(self)
+        guard totalPageNumber > 1 else {
+            return
+        }
+        
+        let currentPageNumber = Int(round(collectionView.contentOffset.x / collectionView.frame.width))
+        
+        if isInfinite {
+            let nextPageNumber = currentPageNumber + 1
+            collectionView.setContentOffset(CGPoint(x: CGFloat(collectionView.frame.width * nextPageNumber), y: 0) , animated: true)
+            
+        }
     }
 
 }
